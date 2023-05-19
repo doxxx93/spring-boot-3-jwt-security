fb293f3185033ad947402d23106a14b0e0e894d7
doxxx <doxxx93@gmail.com>
2023-05-19 09:42:19 +0900

    add UserRepository

---
+++ src/main/java/com/doxxx/security/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.doxxx.security.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
